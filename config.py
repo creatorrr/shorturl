16e9a61afae7f745c87634d1246c11a2397bc9d0
Diwank Singh <diwank.singh@gmail.com>
2016-09-22 16:08:07 +0530

    Meh
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ REDISDB = os.getenv('REDISDB', 0)
 REDISPASSWD = os.getenv('REDISPASSWD', '')
 SITEURL = os.getenv('SITEURL', 'http://example.com')
 KEYLENGTH = os.getenv('KEYLENGTH', 8)
+
+LISTENHOST = os.getenv('LISTENHOST', '127.0.0.1')
+LISTENPORT = os.getenv('LISTENPORT', 5000)
